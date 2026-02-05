d904367a8601b39b55a784b621e86555d5f6606b
Marcelo Luiz <mlluizpereira39@gmail.com>
2026-02-05 13:22:36 -0300

    chore: Update dependencies, integrate Font Awesome, refresh favicons, and rename project.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import AuthWrapper from './lib/AuthWrapper'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('app')).render(
